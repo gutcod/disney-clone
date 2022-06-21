107581562036bfcdb4f5e5e07d67b6de09b775fe
Andrei GUTAN <agutan@P5RO-PORT1706.local>
2022-06-21 22:09:33 +0300

    first commit

---
+++ pages/utils/utils.js
@@ -0,0 +1,16 @@
+export const videosQuerry = `query{
+    videos{
+      createdAt,
+      id,
+      title,
+      description,
+      tags,
+      slug,
+      thumbnail{
+        url
+      }
+      mp4{
+        url
+      }
+    }
+  }`;
